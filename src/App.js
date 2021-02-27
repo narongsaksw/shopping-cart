3cd68ff51e1b81c7de71cd7c658039d38d991613
Narawich Saphimarn (Ball)\Naras <naras529>
2021-02-27 19:55:34 +0700

    update route & css & check null

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from 'react-router-dom';
-import './App.css';
+} from "react-router-dom";
+import "./App.css";
 
 //layouts
-import AdminLayout from './Layout/AdminLayout';
+import AdminLayout from "./Layout/AdminLayout";
 
 //pages
-import LoginPage from './pages/LoginPage';
-import EmployeePage from './pages/EmployeePage';
-import HistoryPage from './pages/HistoryPage';
-import StockPage from './pages/StockPage';
-import EmployeeListPage from './pages/EmployeeListPage';
-import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from "./pages/LoginPage";
+import EmployeePage from "./pages/EmployeePage";
+import HistoryPage from "./pages/HistoryPage";
+import StockPage from "./pages/StockPage";
+import EmployeeListPage from "./pages/EmployeeListPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path='/' exact component={LoginPage} />
-        <Route path={['/dashboard', '/history', '/employee-list', '/stock']}>
+        <Route path="/" exact component={LoginPage} />
+        <Route path={["/dashboard", "/history", "/employee-list", "/stock"]}>
           <AdminLayout>
-            <Route path='/history' component={HistoryPage} />
-            <Route path='/stock' component={StockPage} />
-            <Route path='/employee-list' component={EmployeeListPage} />
+            <Route path="/history" component={HistoryPage} />
+            <Route path="/stock" component={StockPage} />
+            <Route path="/employee-list" component={EmployeeListPage} />
           </AdminLayout>
         </Route>
-        <Route path='/not-found' component={NotFoundPage} />
-        <Redirect to='/not-found' />
+        <Route path="/employee" component={EmployeePage} />
+        <Route path="/not-found" component={NotFoundPage} />
+        <Redirect to="/not-found" />
       </Switch>
     </Router>
   );
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,32 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
 const setUserData = ({ role, name }) => {
-  localStorage.setItem('userData', JSON.stringify({ role, name }));
+  localStorage.setItem("userData", JSON.stringify({ role, name }));
 };
 
 export const isLogin = () => {
-  if (localStorage.getItem('userData')) return true;
+  if (localStorage.getItem("userData")) return true;
   return false;
 };
 
 export const login = async ({ history, username, password }) => {
   await axios
-    .post('https://shopeexpress.herokuapp.com/api/v1/member/login', {
+    .post("https://shopeexpress.herokuapp.com/api/v1/member/login", {
       username,
       password,
     })
     .then((res) => {
-      const {
-        role: { role },
-        user,
-      } = res?.data?.dataValues;
-      setUserData({ role: role, name: user });
+      if (res.data.dataValues != null) {
+        const {
+          role: { role },
+          user,
+        } = res.data.dataValues;
+        setUserData({ role, name: user });
+      } else {
+        history.push("/");
+      }
     });
 
-  if (JSON.parse(localStorage.getItem('userData')).role === 'Admin') {
-    history.push('/dashboard');
+  if (JSON.parse(localStorage.getItem("userData")).role === "Admin") {
+    history.push("/history");
   } else {
-    history.push('/employee');
+    history.push("/employee");
   }
   // console.log({  parse: JSON.parse(getUserData) });
 };
